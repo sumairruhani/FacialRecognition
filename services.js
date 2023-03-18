672fc7a080ab9725cdcbb7bc2ee8c6b58dd62c72
Sumair Ruhani <sumair.ruhani@arup.com>
2023-03-18 16:51:13 +0000

    updated agent

--- services.js
+++ services.js
@@ -1,45 +1,51 @@
 "use strict";
-// Ada Boilerplate - JavaScript and Computer Vision Teachable Machine,
-// Machine Learning & Teachable Machine Models
 
-//for easy lets setup some quick global variables
+//Model URL global variables
 var hairModelURL = "https://teachablemachine.withgoogle.com/models/gf2JtvTHa/"; //variable used to hold path to the model
 var glassesModelURL =
   "https://teachablemachine.withgoogle.com/models/yc7riJ0kE/"; // path to hold model for glasses
 var genderModelURL =
   "https://teachablemachine.withgoogle.com/models/TX87sWJAZ/"; //variable used to hold path to the gender model
 
+// Classifier global variables
 var hairLengthClassifier; //variable used to hold the hair length classifier object
 var glassesClassifier; //variable used to hold the glasses classifier object
 var genderClassifier; //variable used to hold the gender classifier object
 
+// Camera variable
 var cam; //variable used to hold the camera object
 
-var label0 = "",
-  confidence0 = 0; //for ease and just because we're only demo'ing with two classes
-var label1 = "",
-  confidence1 = 0;
+//imported image variable
+var img;
 
-var label2 = "",
-  confidence2 = 0; //for ease and just because we're only demo'ing with two classes
-var label3 = "",
-  confidence3 = 0;
+// Classifier class names label and confidence levels stored in variables
+var labelShort = "",
+  confShort = 0;
+var labelLong = "",
+  confLong = 0;
 
-var label4 = "",
-  confidence4 = 0; //for ease and just because we're only demo'ing with two classes
-var label5 = "",
-  confidence5 = 0;
+var labelGlassesOff = "",
+  confOff = 0;
+var labelGlassesOn = "",
+  confOn = 0;
 
+var labelMale = "",
+  confMale = 0;
+var labelFemale = "",
+  confFemale = 0;
+
+//p5 function - this function is automatically called by the p5 library, once only
 function preload() {
-  //p5 function - this function is automatically called by the p5 library, once only
+  // img = loadImage("Datasets/Gender/Female/000002.jpg");
   hairLengthClassifier = ml5.imageClassifier(hairModelURL + "model.json"); //load the hair model
   glassesClassifier = ml5.imageClassifier(glassesModelURL + "model.json"); //load the glasses model
   genderClassifier = ml5.imageClassifier(genderModelURL + "model.json"); //load the gender model
 }
 
+//p5 function - this function is autmatically called after the 'preload' function; the function is only executed once
 function setup() {
-  //p5 function - this function is autmaticallt called after the 'preload' function; the function is only executed once
   var viewport = createCanvas(480, 360); //p5 function to create a p5 canvas
+  // var viewport = createCanvas(178, 218); //p5 function to create a p5 canvas
   viewport.parent("video_container"); //attach the p5 canvas to the target html div
   frameRate(24); //set the frame rate, we dont need to high performance video
 
@@ -63,10 +69,10 @@ function processresultsHair(error, results) {
     console.error("classifier error: " + error);
   } else {
     //no errors detected, so lets grab the label and execute the classify function again
-    label0 = results[0].label;
-    confidence0 = results[0].confidence;
-    label1 = results[1].label;
-    confidence1 = results[1].confidence;
+    labelShort = results[0].label;
+    confShort = results[0].confidence;
+    labelLong = results[1].label;
+    confLong = results[1].confidence;
     classify(); //execute the classify function again
   }
 }
@@ -78,10 +84,10 @@ function processresultsGlasses(error, results) {
     console.error("classifier error: " + error);
   } else {
     //no errors detected, so lets grab the label and execute the classify function again
-    label2 = results[0].label;
-    confidence2 = results[0].confidence;
-    label3 = results[1].label;
-    confidence3 = results[1].confidence;
+    labelGlassesOff = results[0].label;
+    confOff = results[0].confidence;
+    labelGlassesOn = results[1].label;
+    confOn = results[1].confidence;
     classify(); //execute the classify function again
   }
 }
@@ -93,64 +99,130 @@ function processresultsGender(error, results) {
     console.error("classifier error: " + error);
   } else {
     //no errors detected, so lets grab the label and execute the classify function again
-    label4 = results[0].label;
-    confidence4 = results[0].confidence;
-    label5 = results[1].label;
-    confidence5 = results[1].confidence;
+    labelMale = results[0].label;
+    confMale = results[0].confidence;
+    labelFemale = results[1].label;
+    confFemale = results[1].confidence;
     classify(); //execute the classify function again
   }
 }
 
-function friendlyresults() {
-  //a simple way to return the current classification details
+function friendlyresultsHair() {
+  //A well formatted way to return the hair length confidence levels and the threshold value
   let result = "Please wait...";
-  if (label0.length > 0) {
-    result =
-      label0 +
-      ": " +
-      (confidence0 * 100).toFixed(0) +
-      "%" +
-      ", " +
-      label1 +
-      ": " +
-      (confidence1 * 100).toFixed(0) +
-      "%";
+  if (labelShort && labelLong) {
+    let confidenceLevels = `${labelShort}: ${(confShort * 100).toFixed(
+      0
+    )}%, ${labelLong}: ${(confLong * 100).toFixed(0)}%`;
+
+    switch (true) {
+      case confShort >= 0.8:
+        result = `${confidenceLevels}. Hair is short`;
+        break;
+      case confShort >= 0.6:
+        result = `${confidenceLevels}. Hair is likely to be short`;
+        break;
+      case confShort > 0.5:
+        result = `${confidenceLevels}. Hair could be short`;
+        break;
+      case confLong >= 0.8:
+        result = `${confidenceLevels}. Hair is long`;
+        break;
+      case confLong >= 0.6:
+        result = `${confidenceLevels}. Hair is likely to be long`;
+        break;
+      case confLong > 0.5:
+        result = `${confidenceLevels}. Hair could be long`;
+        break;
+      default:
+        result = `${confidenceLevels}. Hair length cannot be determined`;
+        break;
+    }
+  } else {
+    result = "Error: Hair length cannot be determined";
   }
+
   return result;
 }
 
-function friendlyresults1() {
-  //a simple way to return the current classification details
+function friendlyresultsGlasses() {
+  //A well formatted way to return the glasses confidence levels and the threshold value
+
   let result = "Please wait...";
-  if (label2.length > 0) {
-    result =
-      label2 +
-      ": " +
-      (confidence2 * 100).toFixed(0) +
-      "%" +
-      ", " +
-      label3 +
-      ": " +
-      (confidence3 * 100).toFixed(0) +
-      "%";
+  if (labelGlassesOff && labelGlassesOn) {
+    let confidenceLevels = `${labelGlassesOff}: ${(confOff * 100).toFixed(
+      0
+    )}%, ${labelGlassesOn}: ${(confOn * 100).toFixed(0)}%`;
+
+    switch (true) {
+      case confOff >= 0.8:
+        result = `${confidenceLevels}. Person is not wearing glasses`;
+        break;
+      case confOff >= 0.6:
+        result = `${confidenceLevels}. Person is likely not wearing glasses`;
+        break;
+      case confOff > 0.5:
+        result = `${confidenceLevels}. Person could be not be wearing glasses`;
+        break;
+      case confOn >= 0.8:
+        result = `${confidenceLevels}. Person is wearing glasses`;
+        break;
+      case confOn >= 0.6:
+        result = `${confidenceLevels}. Person is likely to be wearing glasses`;
+        break;
+      case confOn > 0.5:
+        result = `${confidenceLevels}. Person could be wearing glasses`;
+        break;
+      default:
+        result = `${confidenceLevels}. Cannot determine whether glasses are being worn`;
+        break;
+    }
+  } else {
+    result = "Error: Cannot determine whether glasses are being worn";
   }
+
   return result;
 }
 
-function friendlyresults2() {
-  //a simple way to return the current classification details
+var labelMale = "",
+  confMale = 0;
+var labelFemale = "",
+  confFemale = 0;
+
+function friendlyresultsGender() {
+  //A well formatted way to return the glasses confidence levels and the threshold value
+
   let result = "Please wait...";
-  if (label4.length > 0) {
-    result =
-      label4 +
-      ": " +
-      (confidence4 * 100).toFixed(0) +
-      "%" +
-      ", " +
-      label5 +
-      ": " +
-      (confidence5 * 100).toFixed(0) +
-      "%";
+  if (labelMale && labelFemale) {
+    let confidenceLevels = `${labelMale}: ${(confMale * 100).toFixed(
+      0
+    )}%, ${labelFemale}: ${(confFemale * 100).toFixed(0)}%`;
+
+    switch (true) {
+      case confMale >= 0.8:
+        result = `${confidenceLevels}. Person is a male`;
+        break;
+      case confMale >= 0.6:
+        result = `${confidenceLevels}. Person is likely to be a male`;
+        break;
+      case confMale > 0.5:
+        result = `${confidenceLevels}. Person could be a male`;
+        break;
+      case confFemale >= 0.8:
+        result = `${confidenceLevels}. Person is a female`;
+        break;
+      case confFemale >= 0.6:
+        result = `${confidenceLevels}. Person is likely to be a female`;
+        break;
+      case confFemale > 0.5:
+        result = `${confidenceLevels}. Person could be a female`;
+        break;
+      default:
+        result = `${confidenceLevels}. Cannot determine the gender`;
+        break;
+    }
+  } else {
+    result = "Error: Cannot determine the gender";
   }
   return result;
 }
@@ -160,7 +232,8 @@ function draw() {
   background("#c0c0c0"); //set the canvas default back colour
 
   image(cam, 0, 0); //pass the video to the p5 canvas
-  document.getElementById("results").innerHTML = friendlyresults(); //update the result string
-  document.getElementById("results1").innerHTML = friendlyresults1(); //update the result string
-  document.getElementById("results2").innerHTML = friendlyresults2(); //update the result string
+  // image(img, 0, 0);
+  document.getElementById("results").innerHTML = friendlyresultsHair(); //update the result string
+  document.getElementById("results1").innerHTML = friendlyresultsGlasses(); //update the result string
+  document.getElementById("results2").innerHTML = friendlyresultsGender(); //update the result string
 }
